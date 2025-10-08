dd8426ae95e987293ec0887e2e7ab8a4eab5288a
Carlos Henrique <carlosh.sales@outlook.com>
2025-10-08 20:07:53 -0300

    template string, scanner, variables

--- get-started/src/App.java
+++ get-started/src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        //One line comment
+        /**
+         * Many lines comment
+         */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Olá, informe seu nome:");
+        String name = scanner.next();
+        System.out.println("Informe sua idade:");
+        int age = scanner.nextInt();
+
+        //normal format
+        System.out.println("Olá " + name + " sua idade é " + age);
+        //template string
+        System.out.printf("Olá %s sua idade é %s \n", name, age);
     }
 }
