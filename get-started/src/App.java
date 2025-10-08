f5c2ceb1a89b5f8cbe58d8904a34d86b3c16e1e9
Carlos Henrique <carlosh.sales@outlook.com>
2025-10-08 20:23:14 -0300

    excercice1 class

--- get-started/src/App.java
+++ get-started/src/App.java
@@ -1,23 +1,6 @@
-import java.util.Scanner;
-
 public class App {
 
-    private final static String WELCOME_MESSAGE = "Hello World!";
-
     public static void main(String[] args) throws Exception {
-        //One line comment
-        /**
-         * Many lines comment
-         */
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Olá, informe seu nome:");
-        String name = scanner.next();
-        System.out.println("Informe sua idade:");
-        int age = scanner.nextInt();
-
-        //normal format
-        System.out.println("Olá " + name + " sua idade é " + age);
-        //template string
-        System.out.printf("Olá %s sua idade é %s \n", name, age);
+        
     }
 }
