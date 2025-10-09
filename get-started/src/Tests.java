47a0bcd3f6f6146e41c70a4209ef690fa5e2e157
Carlos Henrique <carlosh.sales@outlook.com>
2025-10-09 06:58:01 -0300

    feat: code Start in new archive

--- get-started/src/Tests.java
+++ get-started/src/Tests.java
@@ -2,14 +2,7 @@ import java.util.Scanner;
 
 public class Tests{
     public static void start(){
-        var scanner = new Scanner(System.in);
-        var number = scanner.nextInt();
-
-        var message = switch (number) {
-            case 1 -> "Hello";
-            case 2 -> "World";
-            default -> "Error";
-        }
+        
 
         
     }
